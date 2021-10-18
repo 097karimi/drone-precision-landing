563e9df96c66fde5abbdf0ef6481591d648ffa2b
097karimi <097karimi@gmail.com>
2021-10-18 13:14:05 +0330

    Add vision class

--- src/main.py
+++ src/main.py
@@ -1,52 +1,18 @@
 import cv2 as cv
 import time
-from func import *
+from vision import Vision
+import time
 
+vision = Vision(0)
 
-cap = cv.VideoCapture(0)
-
-while cap.isOpened():
-    tmp = time.time()
-
-    _, frame = cap.read()
-
-    (corners, ids, rejected_area) = cv.aruco.detectMarkers(
-        frame,
-        cv.aruco.Dictionary_get(cv.aruco.DICT_ARUCO_ORIGINAL),
-        parameters=cv.aruco.DetectorParameters_create()
-        )
-
-    cv.aruco.drawDetectedMarkers(frame, corners, borderColor=(0, 0, 255))
-
-
-    error_x, error_y = estimate_error(*center_of_aruco(corners), *center_of_camera(cap))
-    
-    show_on_screen(
-        frame,
-        {
-            'Error-X': error_x,
-            'Error-Y': error_y,
-        }
-    )
-
-    cv.circle(frame, center_of_aruco(corners), 7, (0, 255, 0), -1)
-    cv.circle(frame, center_of_camera(cap), 7, (0, 0, 0), -1)
-    
-
-    show_on_screen(
-        frame,
-        {
-            'TPF : ': time.time() - tmp,
-        },
-        120
-    )
-
-    
-    cv.namedWindow("ArUco", cv.WINDOW_GUI_NORMAL)
-    cv.imshow("ArUco", frame)
-
+while True:
+    vision.detect_markers()
+    x,y = vision.estimate_error()
+    vision.show_text_on_screen({
+        'Error x':x,
+        'Error y': y
+        })
+    vision.show_window()
     if cv.waitKey(100) & 0xFF == ord('q'):
+        del vision
         break
-
-cap.release()
-cv.destroyAllWindows()
---
+++ src/vision.py
@@ -0,0 +1,77 @@
+import cv2 as cv
+
+
+class Vision:
+    def __init__(self,cam=0):
+        self.__cap = cv.VideoCapture(cam)
+
+
+    def __del__(self):
+        self.__cap.release()
+        cv.destroyAllWindows()
+
+
+    def detect_markers(self ,cam=0):
+        if self.__cap.isOpened():
+
+            _, self.__frame = self.__cap.read()
+
+            (self.__corners, self.__ids, self.__rejected_area) = cv.aruco.detectMarkers(
+                self.__frame,
+                cv.aruco.Dictionary_get(cv.aruco.DICT_ARUCO_ORIGINAL),
+                parameters=cv.aruco.DetectorParameters_create()
+                )
+
+            cv.aruco.drawDetectedMarkers(self.__frame, self.__corners, borderColor=(0, 0, 255))
+
+
+    def center_of_aruco(self):
+        if self.__corners == []:
+            return (0,0)
+        for corner in self.__corners:
+            x = (corner[0][0][0] + corner[0][1][0] + corner[0][2][0] + corner[0][3][0]) / 4
+            y = (corner[0][0][1] + corner[0][1][1] + corner[0][2][1] + corner[0][3][1]) / 4
+        return (int(x), int(y))
+
+
+    def center_of_camera(self):
+        x = int(self.__cap.get(cv.CAP_PROP_FRAME_WIDTH)/2)
+        y = int(self.__cap.get(cv.CAP_PROP_FRAME_HEIGHT)/2)
+        return (x, y)
+
+    def cam_resolution(self):
+        x = int(self.__cap.get(cv.CAP_PROP_FRAME_WIDTH))
+        y = int(self.__cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+        return (x, y)
+
+
+    def estimate_error(self):
+        ar_x, ar_y = self.center_of_aruco()
+        cam_x, cam_y = self.center_of_camera()
+        x = int(ar_x) - cam_x
+        y = (int(ar_y) - cam_y) * -1
+        return (x, y)
+
+
+    def show_text_on_screen(self, lable_text_dict, axix_y=20):
+        for k, v in lable_text_dict.items():
+            cv.putText(self.__frame, str(k), (10, axix_y),
+            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+            cv.putText(self.__frame, str(v), (90, axix_y),
+            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+            axix_y += 20
+
+    def draw_circle(self, x,y):
+        cv.circle(self.__frame, (x, y), 7, (0, 255, 0), -1)
+
+
+    def show_window(self):
+        cv.namedWindow("ArUco", cv.WINDOW_GUI_NORMAL)
+        cv.imshow("ArUco", self.__frame)
+        
+    
+
+        
+
